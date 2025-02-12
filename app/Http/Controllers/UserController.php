1117829c38f03037682192a23a497f810a41d3e8
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2025-02-12 21:19:24 +0100

    Register completo + Login mas o menos correcto

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -15,7 +17,22 @@ class UserController extends Controller
     public function doLogin(Request $request) {
         // VALIDAR DATOS DE ENTRADA
 
+        $validator = Validator::make(
+            $request->all(), //Los datos a validar
+            [
+                "email"=> "required",
+                "password"=> "required"
+            ], [
+                "email.required"=>"The :attribute is required.",
+                "password.required"=>"The :attribute is required."
+            ]
+        );
+
         // SI LOS DATOS SON INVÁLIDOS, DEVOLVER A LA PÁGINA ANTERIOR E IMPRIMIR LOS ERRORES DE VALIDACIÓN
+
+        if($validator->fails()) {
+            return redirect()->back()->withErrors($validator);
+        }
 
         // SI LOS DATOS SON VÁLIDOS (SI EL LOGIN ES CORRECTO) CARGAR LA VISTA PRINCIPAL DEL USUARIO.
         // LA VISTA PRINCIPAL DE USUARIO DEBE INCLUIR:
@@ -28,7 +45,28 @@ class UserController extends Controller
             -> Un botón para marcar como hecha la tarea.
         */
 
-        return view('user_views.index'); // CARGA LA VIEW PRINCIPAL CON LA INFO DEL USUARIO
+        $datosLogin = $request->all();
+        $email = $datosLogin["email"];
+        $password = $datosLogin["password"];
+
+        $usuarioBD = User::where("email", $email)->first();
+
+        if ($usuarioBD) {
+
+            if (password_verify($password, $usuarioBD->password)) {
+
+                return view('user_views.index', compact('usuarioBD')); // CARGA LA VIEW PRINCIPAL CON LA INFO DEL USUARIO
+
+            }
+
+        } else {
+
+            $loginIncorrecto = "The login credentials are not correct.";
+
+            return view('user_views.login', compact('loginIncorrecto'));
+
+        }
+
     }
 
     //Show register form
@@ -44,12 +82,46 @@ class UserController extends Controller
             -> nombre es obligatorio, debe ser un string y debe ser menor de 20 carácteres
             -> email es obligatorio, debe seguir un formato estándar, debe ser único en la base de datos
             -> password es obligatoria, debe ser mayor de 5 carácteres, menor de 20 carácteres, debe contener una minúscula, una mayúscula y al menos un dígito
-            -> password_repeat es obligatoria y debe ser igual a password
+            -> repeat_password es obligatoria y debe ser igual a password
         */
+
+        $validator = Validator::make(
+            $request->all(), //Los datos a validar
+            [
+                "name"=> "required|string|max:20",
+                "email"=> "required|email:rfc,dns|unique:users,email",
+                "password"=> "required|min:5|max:20|regex:/[a-z]/|regex:/[A-Z]/|regex:/[0-9]/",
+                "repeat_password"=> "required|same:password"
+            ], [
+                "name.required"=>"The :attribute is required.",
+                "name.string"=>"The :attribute must be a string",
+                "name.max"=>"The :attribute must be less or equal to 20 characters.",
+                "email.required"=>"The :attribute is required.",
+                "email.email"=>"The :attribute must have the right format.",
+                "email.unique"=>"The :attribute must be unique.",
+                "password.required"=>"The :attribute is required.",
+                "password.min"=>"The :attribute must be more or equal to 5 characters.",
+                "password.max"=>"The :attribute must be less or equal to 20 characters.",
+                "password.regex"=>"The :attribute must contain at least one lowercase letter, a capital letter and a numerical digit.",
+                "repeat_password.required"=>"The repeated password is required.",
+                "repeat_password.same"=>"The repeated password is not the same as the original password."
+            ]
+        );
 
         // SI LOS DATOS SON INVÁLIDOS, DEVOLVER A LA PÁGINA ANTERIOR E IMPRIMIR LOS ERRORES DE VALIDACIÓN
 
+        if($validator->fails()) {
+            return redirect()->back()->withErrors($validator);
+        }
+
         // SI LOS DATOS SON VÁLIDOS (SI EL REGISTRO SE HA REALIZADO CORRECTAMENTE) CARGAR LA VIEW DE LOGIN PARA PODER REALIZAR LOGIN
+
+        $user = new User();
+        $user["name"] = $request["name"];
+        $user["email"] = $request["email"];
+        $user["password"] = $request["password"];
+        
+        $user->save();
 
         return view('user_views.login'); // CARGA LA VIEW DE LOGIN PARA PODER REALIZAR LOGIN
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -44,5 +45,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function chores(): HasMany {
+        return $this->hasMany(Chore::class, 'assigned_to', 'id');
     }
 }
